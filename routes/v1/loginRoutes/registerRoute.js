5d8fdaa017c16b5e0af0802e961e71b8fc8c34bb
AgraelPL <kamilkandula@gmail.com>
2021-04-05 14:47:32 +0200

    RegisterForm

--- routes/v1/loginRoutes/registerRoute.js
+++ routes/v1/loginRoutes/registerRoute.js
@@ -3,6 +3,10 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 
+
+// Add reference to User mongoose schema
+const User = require('../../../schemas/User');
+
 app.use(bodyParser.urlencoded({extended:false}))
 
 
@@ -10,18 +14,50 @@ router.get("/",(req,res,next)=>{
     res.status(200).render("register",{layout:'login-layout'});
 })
 
-router.post("/",(req,res,next)=>{
+router.post("/",async (req,res,next)=>{
     
+    // Get request from register form
     let registerBody ={
         username:req.body.userName.trim(),
         email:req.body.email.trim(),
         password:req.body.password.trim()
     }
 
+    // Check if not empty or undefined
+    if(registerBody.username && registerBody.email &&registerBody.password){
+        let user = await User.findOne({
+            
+            $or:[
+                {username:registerBody.username},
+                {email:registerBody.email}
+            ]        
+            
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.status(400).json({message:'error'});
+        })
 
+        if(user == null){
+            // No user found 
+            // You can redirect to homepage
+            User.create(registerBody)
+            .then(user=>{
+                console.log(user);
+            })
+            res.status(200).render("login",{layout:'login-layout'});
+        }
+        else{
+            // Send message to register page
+            // If username => username is used
+            // Else emai => email is used            
 
-    console.log(registerBody);
-    res.status(200).render("login",{layout:'login-layout'});
+            console.log('Data is already used');
+            res.status(200).render('register',{layout:'login-layout'});
+        }       
+    }
+
+    
 })
 
 
--- schemas/User.js
+++ schemas/User.js
@@ -3,23 +3,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    userName:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
-        trim:true
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt:{ 
+        type: Date, 
+        required: true, 
+        default: Date.now 
     }
+    
 })
 
-const User = mongoose.model('User',UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
